0c98542181dfffc2416fca19de01def9ab5cae0a
ANKITKUMAR1809 <skwedsak143@gmail.com>
2025-02-21 12:06:43 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-gray-900 to-black text-white">
-      <div className="bg-zinc-300 bg-opacity-20 backdrop-blur-lg p-12 rounded-3xl shadow-2xl border border-gray-300/30 text-center w-[500px] relative">
+      <div className="bg-zinc-300 bg-opacity-20 backdrop-blur-lg md:p-12 p-4 rounded-3xl shadow-2xl border border-gray-300/30 text-center md:w-[500px] w-[95%] relative">
         <div className="absolute top-2 right-2 text-gray-400 text-sm">⏱️</div>
         <h1 className="text-4xl font-bold mb-6 tracking-widest text-black">Stopwatch</h1>
         <div className="text-4xl font-mono font-semibold bg-black bg-opacity-60 p-6 rounded-2xl shadow-inner mb-8 border border-gray-500 relative">
@@ -34,22 +34,21 @@ export default function App() {
         <div className="flex justify-center space-x-6">
           <button
             onClick={() => setIsRunning(true)}
-            className="px-8 py-4 rounded-lg bg-green-500 hover:bg-green-600 active:scale-95 
-            transition-all text-xl font-bold shadow-lg flex items-center space-x-3"
+            className="md:text-xl text-sm rounded-lg bg-green-500 hover:bg-green-600 active:scale-95 
+            transition-all font-bold shadow-lg flex items-center md:space-x-2 md:p-4 md:px-6 p-2 space-x-1"
           >
             <FaPlay /> <span>Start</span>
           </button>
           <button
             onClick={() => setIsRunning(false)}
-            className="px-8 py-4 rounded-lg bg-red-500 hover:bg-red-600 active:scale-95 transition-all 
-            text-xl font-bold shadow-lg flex items-center space-x-3"
+            className=" md:text-xl text-sm rounded-lg bg-red-500 hover:bg-red-600 active:scale-95 transition-all  font-bold shadow-lg flex items-center md:space-x-2 md:p-4 md:px-6 p-2 space-x-1"
           >
             <FaStop /> <span>Stop</span>
           </button>
           <button
             onClick={() => { setTime(0); setIsRunning(false); }}
-            className="px-8 py-4 rounded-lg bg-blue-500 hover:bg-blue-600 active:scale-95 
-            transition-all text-xl font-bold shadow-lg flex items-center space-x-3"
+            className=" md:text-xl text-sm rounded-lg bg-blue-500 hover:bg-blue-600 active:scale-95 
+            transition-all font-bold shadow-lg flex items-center md:space-x-2 md:p-4 md:px-6 p-2 space-x-1"
           >
             <FaRedo /> <span>Reset</span>
           </button>
